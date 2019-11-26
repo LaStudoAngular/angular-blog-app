f49bcc6e06c676edd484c16cacb37867f9475a5a
Valery Semenenko <gearmobile@gmail.com>
2019-11-26 14:14:15 +0300

    feat: add post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { PostPageComponent } from './components/post-page/post-page.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
-  declarations: [AppComponent, MainLayoutComponent, HomePageComponent, PostPageComponent],
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PostPageComponent,
+    PostComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
